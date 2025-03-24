411ba5f45b746fdbd66e9994738aa204678bf873
unknown <abdullahalrahman23@gmail.com>
2025-03-24 15:27:46 +0300

    add :textfield and label

--- Cryptography_/src/App.java
+++ Cryptography_/src/App.java
@@ -1,5 +1,48 @@
+import java.awt.Color;
+import java.awt.FlowLayout;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // Create a JFrame (window)
+        JFrame frame = new JFrame("Cryptography application");
+        frame.setSize(1080, 720);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null); // Center window
+
+        ////Customize buy myself
+        // frame.getContentPane().setBackground(Color.); ///color set "#009CDE"
+        frame.getContentPane().setBackground(Color.decode("#009CDE")); ///color set "#009CDE"
+
+        ///Create a panel for holding label and input field
+        JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+
+        ///Create and add a Jlabel for text
+        JLabel plainTextLabel = new JLabel("Enter you plain text");
+        // plainTextLabel.setForeground(Color.white);
+
+        ///Create a textinput field
+        JTextField plTextArea = new JTextField(20); // Text field with 20 columns
+
+        // Add the label and text field with gap
+        panel.add(plainTextLabel);
+        panel.add(Box.createVerticalStrut(20)); // Vertical gap of 20 pixels
+        panel.add(plTextArea);
+
+        // Add the panel to the frame
+        frame.add(panel);
+
+        // Make window visible
+        frame.setVisible(true);
     }
 }
