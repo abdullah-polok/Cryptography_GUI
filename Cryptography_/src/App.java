a1f9fa4133fee256b311308a0a2e67f0d61ebc35
unknown <abdullahalrahman23@gmail.com>
2025-03-26 15:34:26 +0300

    add:grid layout for application

--- Cryptography_/src/App.java
+++ Cryptography_/src/App.java
@@ -1,21 +1,22 @@
+import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.FlowLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Insets;
 
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
+import javax.swing.BorderFactory;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
-import javax.swing.border.EmptyBorder;
 
-public class App {
+public class App extends JFrame {
+
     public static void main(String[] args) throws Exception {
 
+        JFrame frame = new JFrame("Cryptography Application");
         // Create a JFrame (window)
-        JFrame frame = new JFrame("Cryptography application");
         frame.setSize(1080, 720);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null); // Center window
@@ -24,23 +25,50 @@ public class App {
         // frame.getContentPane().setBackground(Color.); ///color set "#009CDE"
         frame.getContentPane().setBackground(Color.decode("#009CDE")); ///color set "#009CDE"
 
-        ///Create a panel for holding label and input field
-        JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        ///Create a main panel for centering the grid
+        JPanel mainPanel = new JPanel(new BorderLayout());
 
-        ///Create and add a Jlabel for text
-        JLabel plainTextLabel = new JLabel("Enter you plain text");
-        // plainTextLabel.setForeground(Color.white);
+        mainPanel.setBackground(Color.decode("#009CDE"));
+        ///Create Panel with gridLayout
+        JPanel gridJPanel = new JPanel(new GridLayout(0, 2, 0, 10));
 
-        ///Create a textinput field
-        JTextField plTextArea = new JTextField(20); // Text field with 20 columns
+        ///make custom height for text field and selection field
+        Dimension inputFieldSize = new Dimension(120, 2);
 
-        // Add the label and text field with gap
-        panel.add(plainTextLabel);
-        panel.add(Box.createVerticalStrut(20)); // Vertical gap of 20 pixels
-        panel.add(plTextArea);
+        ///Create label and text field
+        JLabel plaintextLabel = new JLabel("Entyer your plain text");
+        JTextField plainTextField = new JTextField(10);
 
-        // Add the panel to the frame
-        frame.add(panel);
+        gridJPanel.add(plaintextLabel);
+        gridJPanel.add(plainTextField);
+        plainTextField.setPreferredSize(inputFieldSize);
+        ///reduced internal padding of text field
+        plainTextField.setMargin(new Insets(2, 5, 2, 5));
+
+        ///Create label and selection input field
+        JLabel selectAlgoLabel = new JLabel("Select your algorithm");
+        String[] algoString = new String[] { "AES", "DES", "RSA" };
+        JComboBox selectComboBox = new JComboBox<>(algoString);
+        gridJPanel.add(selectAlgoLabel);
+        gridJPanel.add(selectComboBox);
+
+        ///Create label and key input value
+        gridJPanel.add(new JLabel("Enter the key"));
+        gridJPanel.add(new JTextField(10));
+
+        ///Create label and output field
+        gridJPanel.add(new JLabel("Cipher text"));
+        gridJPanel.add(new JTextField(10));
+
+        // Wrap the grid in an empty border to create margin
+        gridJPanel.setBorder(BorderFactory.createEmptyBorder(40, 40, 40, 40));
+
+        ///Center the main grid using wrapper panel
+
+        mainPanel.add(gridJPanel, BorderLayout.CENTER);
+
+        ///Add frame into the panel
+        frame.add(mainPanel);
 
         // Make window visible
         frame.setVisible(true);
